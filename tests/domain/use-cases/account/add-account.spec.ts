9a9ebc3324b24eece3fe2bfb36ab55f3051843d2
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-04-20 12:45:09 -0300

    test: ensure AddAccountUseCase throw FieldInUseError if HttpClient return 400

--- tests/domain/use-cases/account/add-account.spec.ts
+++ tests/domain/use-cases/account/add-account.spec.ts
@@ -3,12 +3,17 @@ import { type HttpClient } from '@/domain/contracts/http'
 import { AccountParams, httpClientParams } from '@/tests/mocks'
 
 import { mock } from 'jest-mock-extended'
+import { FieldInUseError } from '@/domain/errors'
 
 describe('AddAccountUseCase', () => {
   let sut: AddAccount
   const { url } = httpClientParams
   const { name, email, password } = AccountParams
   const httpClient = mock<HttpClient>()
+
+  beforeAll(() => {
+    httpClient.request.mockResolvedValue({ statusCode: 200 })
+  })
 
   beforeEach(() => {
     sut = AddAccountUseCase(url, httpClient)
@@ -19,5 +24,13 @@ describe('AddAccountUseCase', () => {
 
     expect(httpClient.request).toHaveBeenCalledWith({ url, method: 'post', body: { name, email, password } })
     expect(httpClient.request).toHaveBeenCalledTimes(1)
+  })
+
+  it('should throw FieldInUseError if HttpClient return 400', async () => {
+    httpClient.request.mockResolvedValueOnce({ statusCode: 400 })
+
+    const promise = sut({ name, email, password })
+
+    await expect(promise).rejects.toThrow(new FieldInUseError('email'))
   })
 })
