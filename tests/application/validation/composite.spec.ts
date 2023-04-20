ac23d0c94b626282bbc8877f5debfd9250410968
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-04-20 16:11:35 -0300

    test: ensure ValidationComposite return first error if any Validator fails

---
+++ tests/application/validation/composite.spec.ts
@@ -0,0 +1,35 @@
+import { type FieldValidator, ValidationComposite } from '@/application/validation'
+
+import { type MockProxy, mock } from 'jest-mock-extended'
+import faker from 'faker'
+
+describe('ValidationComposite', () => {
+  let sut: ValidationComposite
+  let validator1: MockProxy<FieldValidator>
+  let validator2: MockProxy<FieldValidator>
+  let validators: FieldValidator[]
+  let fieldName: string
+  let value: string
+
+  beforeAll(() => {
+    fieldName = faker.database.column()
+    value = faker.random.word()
+    validator1 = mock({ field: fieldName })
+    validator2 = mock({ field: fieldName })
+    validators = [validator1, validator2]
+  })
+
+  beforeEach(() => {
+    sut = ValidationComposite.build(validators)
+  })
+
+  it('should return first error if any Validator fails', () => {
+    const error = new Error('first error')
+    validator1.validate.mockReturnValueOnce(error)
+    validator2.validate.mockReturnValueOnce(new Error('other error'))
+
+    const validateError = sut.validate(fieldName, { [fieldName]: value })
+
+    expect(validateError).toBe(error.message)
+  })
+})
