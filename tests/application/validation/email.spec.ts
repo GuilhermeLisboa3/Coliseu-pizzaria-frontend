e21dd65dc1e2ba56a264485a27ce4609412d1737
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-04-20 14:20:59 -0300

    test: ensure EmailValidation return undefined if email is valid

--- tests/application/validation/email.spec.ts
+++ tests/application/validation/email.spec.ts
@@ -6,10 +6,12 @@ import faker from 'faker'
 describe('EmailValidation', () => {
   let fieldName: string
   let invalidEmail: string
+  let validEmail: string
 
   beforeAll(() => {
     fieldName = faker.database.column()
     invalidEmail = faker.random.word()
+    validEmail = faker.internet.email()
   })
 
   it('should return error if email is invalid', () => {
@@ -24,6 +26,14 @@ describe('EmailValidation', () => {
     const sut = new EmailValidation(fieldName)
 
     const error = sut.validate({ [fieldName]: undefined })
+
+    expect(error).toBeFalsy()
+  })
+
+  it('should return undefined if email is valid', () => {
+    const sut = new EmailValidation(fieldName)
+
+    const error = sut.validate({ [fieldName]: validEmail })
 
     expect(error).toBeFalsy()
   })
