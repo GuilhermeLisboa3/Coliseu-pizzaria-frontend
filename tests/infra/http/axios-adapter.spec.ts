46953aeeacc8bda2505cd4225d83a240952b4248
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-04-20 13:41:13 -0300

    test: ensure AxiosAdapter return correct response

--- tests/infra/http/axios-adapter.spec.ts
+++ tests/infra/http/axios-adapter.spec.ts
@@ -7,8 +7,12 @@ jest.mock('axios')
 
 describe('AxiosAdapter', () => {
   const fakeAxios = axios as jest.Mocked<typeof axios>
-  const { method, body, url } = httpClientParams
+  const { method, body, url, statusCode, data } = httpClientParams
   let sut: AxiosAdapter
+
+  beforeAll(() => {
+    fakeAxios.request.mockResolvedValue({ status: statusCode, data })
+  })
 
   beforeEach(() => {
     sut = new AxiosAdapter()
@@ -17,6 +21,14 @@ describe('AxiosAdapter', () => {
   it('should call axios with correct value', async () => {
     await sut.request({ method, body, url })
 
-    expect(fakeAxios).toHaveBeenCalledWith({ method, data: body, url })
+    expect(fakeAxios.request).toHaveBeenCalledWith({ method, data: body, url })
+    expect(fakeAxios.request).toHaveBeenCalledTimes(1)
+  })
+
+  it('should return correct response', async () => {
+    const response = await sut.request({ method, body, url })
+
+    expect(response.statusCode).toBe(statusCode)
+    expect(response.data).toBe(data)
   })
 })
