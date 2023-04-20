327cdcc64f348cc89ec1a0f5d624f0cb286fb745
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-04-20 13:03:43 -0300

    feat: ensure AddAccountUseCase return true if HttpClient return 200

--- src/domain/use-cases/account/add-account.ts
+++ src/domain/use-cases/account/add-account.ts
@@ -1,15 +1,16 @@
 import { type HttpClient } from '@/domain/contracts/http'
 import { FieldInUseError, UnexpectedError } from '@/domain/errors'
 
-type Setup = (url: string, httpClient: HttpClient) => AddAccount
+type Setup = (url: string, httpClient: HttpClient<boolean>) => AddAccount
 type Input = { name: string, email: string, password: string }
-export type AddAccount = (input: Input) => Promise<void>
+type Output = boolean
+export type AddAccount = (input: Input) => Promise<Output>
 
 export const AddAccountUseCase: Setup = (url, httpClient) => async ({ email, name, password }) => {
-  const { statusCode } = await httpClient.request({ url, method: 'post', body: { email, name, password } })
+  const { statusCode, data } = await httpClient.request({ url, method: 'post', body: { email, name, password } })
 
   switch (statusCode) {
-    case 200: return undefined
+    case 200: return data!
     case 400: throw new FieldInUseError('email')
     default: throw new UnexpectedError()
   }
