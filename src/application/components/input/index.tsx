922437f67dc5f83a8a5235a993024f4d66b6fef3
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-07-14 13:46:01 -0300

    feat: ensure SignUp call validation with correct values

--- src/application/components/input/index.tsx
+++ src/application/components/input/index.tsx
@@ -2,13 +2,24 @@ import { Container } from './style'
 
 import React from 'react'
 
-type Props = React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
+type Props = React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement> & {
+  setState: React.Dispatch<React.SetStateAction<string>>
+}
 
-export const Input: React.FC<Props> = ({ name, placeholder, value, ...props }): JSX.Element => {
+export const Input: React.FC<Props> = ({ name, placeholder, value, setState, ...props }): JSX.Element => {
   return (
   <>
     <Container>
-      <input {...props} placeholder=" " id={name} autoComplete="off" type='text' value={value}/>
+      <input
+      {...props}
+      placeholder=" "
+      id={name}
+      autoComplete="off"
+      type='text'
+      value={value}
+      data-testid={name}
+      onChange={e => { setState(e.target.value) } }
+      />
       <label htmlFor={name}>{ placeholder }</label>
     </Container>
   </>
--- src/application/pages/signup/index.tsx
+++ src/application/pages/signup/index.tsx
@@ -1,11 +1,24 @@
 import { Container, Hyperlink } from './style'
 import { Input, Button, Toast } from '@/application/components'
+import { type Validator } from '@/application/validation'
 import imgSignup from '@/application/assets/img-signup.jpg'
 import logo from '@/application/assets/logo.png'
 
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-export const SignUp = (): JSX.Element => {
+type Props = { validation: Validator }
+
+export const SignUp: React.FC<Props> = ({ validation }): JSX.Element => {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [passwordConfirmation, setPasswordConfirmation] = useState('')
+
+  useEffect(() => { validation.validate('name', { name }) }, [name])
+  useEffect(() => { validation.validate('email', { email }) }, [email])
+  useEffect(() => { validation.validate('password', { password }) }, [password])
+  useEffect(() => { validation.validate('passwordConfirmation', { password, passwordConfirmation }) }, [passwordConfirmation])
+
   return (
     <>
       <Container>
@@ -14,10 +27,10 @@ export const SignUp = (): JSX.Element => {
           <div>
             <img src={logo.src} alt="logo" />
             <form action="">
-              <Input placeholder="Name" type='text' name='name'/>
-              <Input placeholder="Email" type='email' name='email'/>
-              <Input placeholder="Senha" type='password' name='password'/>
-              <Input placeholder="Confirma senha" type='password' name='passwordConfirmation'/>
+              <Input placeholder="Name" type='text' name='name' setState={setName}/>
+              <Input placeholder="Email" type='email' name='email' setState={setEmail}/>
+              <Input placeholder="Senha" type='password' name='password' setState={setPassword}/>
+              <Input placeholder="Confirma senha" type='password' name='passwordConfirmation' setState={setPasswordConfirmation}/>
               <Button type='submit' disabled={true}>Cadastre-se</Button>
             </form>
             <p>Já tem uma conta? <Hyperlink href={'/login'}>Entrar</Hyperlink></p>
