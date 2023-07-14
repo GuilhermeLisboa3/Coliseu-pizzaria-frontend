fcdb0a53184afe23a985cc04f149eb5cca230086
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-07-14 15:42:39 -0300

    test: ensure SignUpValidationFactory make ValidationComposite with correct validations

---
+++ tests/main/factories/application/validation/signup.spec.ts
@@ -0,0 +1,18 @@
+import { makeSignUpValidation } from '@/main/factories/application/validation'
+import { CompareValidation, EmailValidation, LengthValidation, RequiredValidation, ValidationComposite } from '@/application/validation'
+
+describe('SignUpValidationFactory', () => {
+  it('should make ValidationComposite with correct validations', () => {
+    const composite = makeSignUpValidation()
+
+    expect(composite).toEqual(ValidationComposite.build([
+      new RequiredValidation('name'),
+      new RequiredValidation('email'),
+      new EmailValidation('email'),
+      new RequiredValidation('password'),
+      new LengthValidation('password', 5),
+      new RequiredValidation('passwordConfirmation'),
+      new CompareValidation('passwordConfirmation', 'password')
+    ]))
+  })
+})
