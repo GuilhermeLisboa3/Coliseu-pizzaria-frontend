256a9dd619e6f1e8c52ac761d0eead5734af87af
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-07-17 11:08:47 -0300

    feat: ensure AuthenticationUseCase throw UnexpectedError if HttpClient return 500

--- src/domain/use-cases/account/authentication.ts
+++ src/domain/use-cases/account/authentication.ts
@@ -1,5 +1,5 @@
 import { type HttpClient } from '@/domain/contracts/http'
-import { InvalidCredentialsError } from '@/domain/errors'
+import { InvalidCredentialsError, UnexpectedError } from '@/domain/errors'
 
 type Setup = (url: string, httpClient: HttpClient) => Authentication
 type Input = { email: string, password: string }
@@ -10,7 +10,8 @@ export const authenticationUseCase: Setup = (url, httpClient) => async ({ email,
   const { statusCode } = await httpClient.request({ url, method: 'post', body: { email, password } })
 
   switch (statusCode) {
+    case 200: return undefined
     case 401: throw new InvalidCredentialsError()
-    default: return undefined
+    default: throw new UnexpectedError()
   }
 }
