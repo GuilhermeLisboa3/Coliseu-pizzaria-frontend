bf0112baf86d80edca8e378e0ea2e7441c6cf09f
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-07-17 12:31:34 -0300

    test: ensure Login call authentication with correct values

--- tests/application/pages/login.spec.tsx
+++ tests/application/pages/login.spec.tsx
@@ -2,18 +2,19 @@ import { Login } from '@/application/pages'
 import { populateField, AccountParams } from '@/tests/mocks'
 import { type Validator } from '@/application/validation'
 
-import { render, screen } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
 import { mock } from 'jest-mock-extended'
 import React from 'react'
 
 describe('Login', () => {
   const { email, password, error } = AccountParams
   const validator = mock<Validator>()
+  const authentication = jest.fn()
 
   const makeSut = (): void => {
     render(
       <>
-        <Login validation={validator}/>
+        <Login validation={validator} authentication={authentication}/>
       </>
     )
   }
@@ -21,6 +22,11 @@ describe('Login', () => {
   const populateFields = (): void => {
     populateField('email', email)
     populateField('password', password)
+  }
+
+  const simulateSubmit = (): void => {
+    populateFields()
+    fireEvent.click(screen.getByRole('button'))
   }
 
   it('should load with correct initial state', () => {
@@ -48,5 +54,13 @@ describe('Login', () => {
     populateFields()
 
     expect(screen.getByRole('button')).toBeDisabled()
+  })
+
+  it('should call authentication with correct values', () => {
+    makeSut()
+
+    simulateSubmit()
+
+    expect(authentication).toHaveBeenCalledWith({ email, password })
   })
 })
