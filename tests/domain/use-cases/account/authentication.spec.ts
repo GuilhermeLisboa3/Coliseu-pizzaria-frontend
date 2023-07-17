3bf96b107692e5bf083f0dc791ad4a2b8195c60c
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-07-17 11:05:40 -0300

    test: ensure AuthenticationUseCase throw InvalidCredentialsError if HttpClient return 401

--- tests/domain/use-cases/account/authentication.spec.ts
+++ tests/domain/use-cases/account/authentication.spec.ts
@@ -1,6 +1,7 @@
 import { type Authentication, authenticationUseCase } from '@/domain/use-cases/account'
 import { type HttpClient } from '@/domain/contracts/http'
 import { AccountParams, httpClientParams } from '@/tests/mocks'
+import { InvalidCredentialsError } from '@/domain/errors'
 
 import { mock } from 'jest-mock-extended'
 
@@ -9,6 +10,10 @@ describe('authenticationUseCase', () => {
   const { url } = httpClientParams
   const { email, password } = AccountParams
   const httpClient = mock<HttpClient>()
+
+  beforeAll(() => {
+    httpClient.request.mockResolvedValue({ statusCode: 200 })
+  })
 
   beforeEach(() => {
     sut = authenticationUseCase(url, httpClient)
@@ -19,5 +24,13 @@ describe('authenticationUseCase', () => {
 
     expect(httpClient.request).toHaveBeenCalledWith({ url, method: 'post', body: { email, password } })
     expect(httpClient.request).toHaveBeenCalledTimes(1)
+  })
+
+  it('should throw InvalidCredentialsError if HttpClient return 401', async () => {
+    httpClient.request.mockResolvedValueOnce({ statusCode: 401 })
+
+    const promise = sut({ email, password })
+
+    await expect(promise).rejects.toThrow(new InvalidCredentialsError())
   })
 })
