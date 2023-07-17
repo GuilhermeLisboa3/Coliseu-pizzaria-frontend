5d9df17fc44a46a13588c6fc97cf8dde1bd3ebf3
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-07-17 11:10:26 -0300

    test: ensure AuthenticationUseCase throw UnexpectedError if HttpClient return 500

--- tests/domain/use-cases/account/authentication.spec.ts
+++ tests/domain/use-cases/account/authentication.spec.ts
@@ -1,7 +1,7 @@
 import { type Authentication, authenticationUseCase } from '@/domain/use-cases/account'
 import { type HttpClient } from '@/domain/contracts/http'
 import { AccountParams, httpClientParams } from '@/tests/mocks'
-import { InvalidCredentialsError } from '@/domain/errors'
+import { InvalidCredentialsError, UnexpectedError } from '@/domain/errors'
 
 import { mock } from 'jest-mock-extended'
 
@@ -32,5 +32,13 @@ describe('authenticationUseCase', () => {
     const promise = sut({ email, password })
 
     await expect(promise).rejects.toThrow(new InvalidCredentialsError())
+  })
+
+  it('should throw UnexpectedError if HttpClient return 500', async () => {
+    httpClient.request.mockResolvedValueOnce({ statusCode: 500 })
+
+    const promise = sut({ email, password })
+
+    await expect(promise).rejects.toThrow(new UnexpectedError())
   })
 })
