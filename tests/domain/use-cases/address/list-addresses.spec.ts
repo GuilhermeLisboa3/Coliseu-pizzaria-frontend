04c2e1243618f6507fd3ac4eb1c8f6922b1b2401
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-07-21 15:50:47 -0300

    test: ensure listAddressesUseCase throw InvalidCredentialsError if HttpClient return 401

--- tests/domain/use-cases/address/list-addresses.spec.ts
+++ tests/domain/use-cases/address/list-addresses.spec.ts
@@ -3,11 +3,16 @@ import { type HttpClient } from '@/domain/contracts/http'
 import { httpClientParams } from '@/tests/mocks'
 
 import { mock } from 'jest-mock-extended'
+import { InvalidCredentialsError } from '@/domain/errors'
 
 describe('listAddressesUseCase', () => {
   const { url } = httpClientParams
   let sut: ListAddresses
   const httpClient = mock<HttpClient>()
+
+  beforeAll(() => {
+    httpClient.request.mockResolvedValue({ statusCode: 200 })
+  })
 
   beforeEach(() => {
     sut = listAddressesUseCase(url, httpClient)
@@ -18,5 +23,13 @@ describe('listAddressesUseCase', () => {
 
     expect(httpClient.request).toHaveBeenCalledWith({ url, method: 'get' })
     expect(httpClient.request).toHaveBeenCalledTimes(1)
+  })
+
+  it('should throw InvalidCredentialsError if HttpClient return 401', async () => {
+    httpClient.request.mockResolvedValueOnce({ statusCode: 401 })
+
+    const promise = sut()
+
+    await expect(promise).rejects.toThrow(new InvalidCredentialsError())
   })
 })
