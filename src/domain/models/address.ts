efc312d41f16ac507ef86f3f5684b0b16a7f1d66
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-07-21 15:59:52 -0300

    feat: ensure listAddressesUseCase return address if HttpClient return 200

---
+++ src/domain/models/address.ts
@@ -0,0 +1,10 @@
+export type Address = {
+  id: string
+  surname: string
+  zipCode: string
+  neighborhood: string
+  street: string
+  number: number
+  complement: string
+  active: boolean
+}
--- src/domain/use-cases/address/list-addresses.ts
+++ src/domain/use-cases/address/list-addresses.ts
@@ -1,14 +1,15 @@
 import { type HttpClient } from '@/domain/contracts/http'
 import { InvalidCredentialsError, UnexpectedError } from '@/domain/errors'
+import { type Address } from '@/domain/models'
 
-type Setup = (url: string, httpClient: HttpClient) => ListAddresses
-type Output = void
+type Setup = (url: string, httpClient: HttpClient<Address[]>) => ListAddresses
+type Output = Address[]
 export type ListAddresses = () => Promise<Output>
 
 export const listAddressesUseCase: Setup = (url, httpClient) => async () => {
-  const { statusCode } = await httpClient.request({ url, method: 'get' })
+  const { statusCode, data } = await httpClient.request({ url, method: 'get' })
   switch (statusCode) {
-    case 200: return undefined
+    case 200: return data!
     case 401: throw new InvalidCredentialsError()
     default: throw new UnexpectedError()
   }
