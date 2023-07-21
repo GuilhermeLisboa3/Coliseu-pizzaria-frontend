c4aca872cc386fe3faea13463e1be8ea83420b73
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-07-21 16:35:32 -0300

    feat: ensure currentAccount call getCurrentAccountAdapter with correct values

--- src/main/adapters/current-account.ts
+++ src/main/adapters/current-account.ts
@@ -2,3 +2,6 @@ import { type Account } from '@/domain/models'
 import { makeLocalStorageAdapter } from '@/main/factories/infra/cache'
 
 export const setCurrentAccountAdapter = (account: Account): void => { makeLocalStorageAdapter().set({ key: 'account', value: account }) }
+
+export const getCurrentAccountAdapter = (): Account =>
+  makeLocalStorageAdapter().get({ key: 'account' })
