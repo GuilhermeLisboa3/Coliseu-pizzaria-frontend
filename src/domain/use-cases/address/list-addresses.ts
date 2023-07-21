f6ac985c0535e64bf2e87e11f8a78b87de2872aa
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-07-21 15:50:06 -0300

    feat: ensure listAddressesUseCase throw InvalidCredentialsError if HttpClient return 401

--- src/domain/use-cases/address/list-addresses.ts
+++ src/domain/use-cases/address/list-addresses.ts
@@ -1,9 +1,14 @@
 import { type HttpClient } from '@/domain/contracts/http'
+import { InvalidCredentialsError } from '@/domain/errors'
 
 type Setup = (url: string, httpClient: HttpClient) => ListAddresses
 type Output = void
 export type ListAddresses = () => Promise<Output>
 
 export const listAddressesUseCase: Setup = (url, httpClient) => async () => {
-  await httpClient.request({ url, method: 'get' })
+  const { statusCode } = await httpClient.request({ url, method: 'get' })
+  switch (statusCode) {
+    case 401: throw new InvalidCredentialsError()
+    default: return undefined
+  }
 }
