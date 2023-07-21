c25f0b73a1fb7dda4151266edc0df598764dbb06
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-07-21 16:55:27 -0300

    test: ensure AuthorizeHttpClientDecorator add headers to HttpClient

--- tests/main/decorators/authorize-http-client-decorator.spec.ts
+++ tests/main/decorators/authorize-http-client-decorator.spec.ts
@@ -1,4 +1,4 @@
-import { httpClientParams } from '@/tests/mocks'
+import { httpClientParams, AccountParams } from '@/tests/mocks'
 import { AuthorizeHttpClientDecorator } from '@/main/decorators'
 import { type GetStorage } from '@/domain/contracts/cache'
 import { type HttpClient } from '@/domain/contracts/http'
@@ -8,10 +8,15 @@ import { mock } from 'jest-mock-extended'
 describe('AuthorizeHttpClientDecorator', () => {
   let sut: AuthorizeHttpClientDecorator
 
+  const { name, accessToken } = AccountParams
   const { url, method, body, headers } = httpClientParams
 
   const getStorage = mock<GetStorage>()
   const httpClient = mock<HttpClient>()
+
+  beforeAll(() => {
+    getStorage.get.mockReturnValue({ name, accessToken })
+  })
 
   beforeEach(() => {
     sut = new AuthorizeHttpClientDecorator(getStorage, httpClient)
@@ -29,5 +34,11 @@ describe('AuthorizeHttpClientDecorator', () => {
     await sut.request({ url, method })
 
     expect(httpClient.request).toHaveBeenCalledWith({ url, method })
+  })
+
+  it('should add headers to HttpClient', async () => {
+    await sut.request({ url, method })
+
+    expect(httpClient.request).toHaveBeenCalledWith({ url, method, headers: { authorization: `Bearer: ${accessToken}` } })
   })
 })
