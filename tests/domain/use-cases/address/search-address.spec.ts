71b036fc0575aa413359f63af3e6ccaa6207a5c0
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-07-24 11:43:41 -0300

    test: ensure searchAddressUseCase throw FieldNotFoundError if HttpClient return 400

--- tests/domain/use-cases/address/search-address.spec.ts
+++ tests/domain/use-cases/address/search-address.spec.ts
@@ -1,6 +1,7 @@
 import { type SearchAddress, searchAddressUseCase } from '@/domain/use-cases/address'
 import { type HttpClient } from '@/domain/contracts/http'
 import { httpClientParams, addressParams } from '@/tests/mocks'
+import { FieldNotFoundError } from '@/domain/errors'
 
 import { mock } from 'jest-mock-extended'
 
@@ -9,6 +10,10 @@ describe('searchAddressUseCase', () => {
   const { zipCode } = addressParams
   let sut: SearchAddress
   const httpClient = mock<HttpClient>()
+
+  beforeAll(() => {
+    httpClient.request.mockResolvedValue({ statusCode: 200, data: null })
+  })
 
   beforeEach(() => {
     sut = searchAddressUseCase(url, httpClient)
@@ -19,5 +24,13 @@ describe('searchAddressUseCase', () => {
 
     expect(httpClient.request).toHaveBeenCalledWith({ url: `${url}/${zipCode}`, method: 'post' })
     expect(httpClient.request).toHaveBeenCalledTimes(1)
+  })
+
+  it('should throw FieldNotFoundError if HttpClient return 400', async () => {
+    httpClient.request.mockResolvedValueOnce({ statusCode: 400 })
+
+    const promise = sut({ zipCode })
+
+    await expect(promise).rejects.toThrow(new FieldNotFoundError('cep'))
   })
 })
