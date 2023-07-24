74e09cbef92771dd417437598d9f74817f2b337b
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-07-24 12:32:41 -0300

    test: ensure addAddressUseCase throw UnauthorizedError if HttpClient return 401

--- tests/domain/use-cases/address/add-address.spec.ts
+++ tests/domain/use-cases/address/add-address.spec.ts
@@ -3,12 +3,17 @@ import { type HttpClient } from '@/domain/contracts/http'
 import { httpClientParams, addressParams } from '@/tests/mocks'
 
 import { mock } from 'jest-mock-extended'
+import { UnauthorizedError } from '@/domain/errors'
 
 describe('addAddressUseCase', () => {
   const { url } = httpClientParams
   const { zipCode, complement, neighborhood, number, street, surname } = addressParams
   let sut: AddAddress
   const httpClient = mock<HttpClient>()
+
+  beforeAll(() => {
+    httpClient.request.mockResolvedValue({ statusCode: 200, data: null })
+  })
 
   beforeEach(() => {
     sut = addAddressUseCase(url, httpClient)
@@ -19,5 +24,13 @@ describe('addAddressUseCase', () => {
 
     expect(httpClient.request).toHaveBeenCalledWith({ url, method: 'post', body: { zipCode, complement, neighborhood, number, street, surname } })
     expect(httpClient.request).toHaveBeenCalledTimes(1)
+  })
+
+  it('should throw UnauthorizedError if HttpClient return 401', async () => {
+    httpClient.request.mockResolvedValueOnce({ statusCode: 401 })
+
+    const promise = sut({ zipCode, complement, neighborhood, number, street, surname })
+
+    await expect(promise).rejects.toThrow(new UnauthorizedError())
   })
 })
