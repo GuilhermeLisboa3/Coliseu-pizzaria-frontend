07e9107c1be8abba248e1aa4bab5b8f61132e85e
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-07-24 11:49:42 -0300

    feat: ensure searchAddressUseCase return address on success

--- src/domain/use-cases/address/search-address.ts
+++ src/domain/use-cases/address/search-address.ts
@@ -1,15 +1,15 @@
 import { type HttpClient } from '@/domain/contracts/http'
 import { FieldNotFoundError, UnauthorizedError, UnexpectedError } from '@/domain/errors'
 
-type Setup = (url: string, httpClient: HttpClient) => SearchAddress
+type Setup = (url: string, httpClient: HttpClient<{ neighborhood: string, street: string }>) => SearchAddress
 type Input = { zipCode: string }
-type Output = void
+type Output = { neighborhood: string, street: string }
 export type SearchAddress = (input: Input) => Promise<Output>
 
 export const searchAddressUseCase: Setup = (url, httpClient) => async ({ zipCode }) => {
-  const { statusCode } = await httpClient.request({ url: `${url}/${zipCode}`, method: 'post' })
+  const { statusCode, data } = await httpClient.request({ url: `${url}/${zipCode}`, method: 'post' })
   switch (statusCode) {
-    case 200: return undefined
+    case 200: return data!
     case 400: throw new FieldNotFoundError('cep')
     case 401: throw new UnauthorizedError()
     default: throw new UnexpectedError()
