3f2a96e4a7d330a7a2e10440f50d21d27429a86f
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-07-24 16:44:22 -0300

    test: ensure AddAdress call validation with correct values

--- tests/application/pages/profile/add-address/add-address.spec.tsx
+++ tests/application/pages/profile/add-address/add-address.spec.tsx
@@ -1,16 +1,25 @@
+import { populateField, addressParams } from '@/tests/mocks'
 import { AddAddress } from '@/application/pages'
 import { AccountContext } from '@/application/contexts'
+import { type Validator } from '@/application/validation'
 
 import { render, screen } from '@testing-library/react'
 import React from 'react'
+import { mock } from 'jest-mock-extended'
 
 describe('AddAddress', () => {
+  const { zipCode } = addressParams
+  const validator = mock<Validator>()
+
   const makeSut = (): void => {
     render(
       <AccountContext.Provider value={{ setCurrentAccount: jest.fn(), getCurrentAccount: jest.fn() }}>
-        <AddAddress />
+        <AddAddress validation={validator} />
       </AccountContext.Provider>
     )
+  }
+  const populateFields = (): void => {
+    populateField('cep', zipCode)
   }
 
   it('should load with correct initial state', async () => {
@@ -19,5 +28,13 @@ describe('AddAddress', () => {
     expect(screen.getByTestId('cep')).toBeInTheDocument()
     expect(screen.queryByTestId('surname')).not.toBeInTheDocument()
     expect(screen.getByRole('button', { name: 'Buscar' })).toBeDisabled()
+  })
+
+  it('should call validation with correct values', () => {
+    makeSut()
+
+    populateFields()
+
+    expect(validator.validate).toHaveBeenCalledWith('zipCode', { zipCode })
   })
 })
