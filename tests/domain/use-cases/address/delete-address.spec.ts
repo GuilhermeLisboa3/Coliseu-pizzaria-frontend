bfdffbb12edbcc5385f853290ee13ac33c52d400
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-07-25 13:16:17 -0300

    test: ensure deleteAddressUseCase throw UnauthorizedError if HttpClient return 401

--- tests/domain/use-cases/address/delete-address.spec.ts
+++ tests/domain/use-cases/address/delete-address.spec.ts
@@ -3,12 +3,17 @@ import { type HttpClient } from '@/domain/contracts/http'
 import { httpClientParams, addressParams } from '@/tests/mocks'
 
 import { mock } from 'jest-mock-extended'
+import { UnauthorizedError } from '@/domain/errors'
 
 describe('deleteAddressUseCase', () => {
   const { url } = httpClientParams
   const { id } = addressParams
   let sut: DeleteAddress
   const httpClient = mock<HttpClient>()
+
+  beforeAll(() => {
+    httpClient.request.mockResolvedValue({ statusCode: 204, data: null })
+  })
 
   beforeEach(() => {
     sut = deleteAddressUseCase(url, httpClient)
@@ -19,5 +24,13 @@ describe('deleteAddressUseCase', () => {
 
     expect(httpClient.request).toHaveBeenCalledWith({ url: `${url}/${id}`, method: 'delete' })
     expect(httpClient.request).toHaveBeenCalledTimes(1)
+  })
+
+  it('should throw UnauthorizedError if HttpClient return 401', async () => {
+    httpClient.request.mockResolvedValueOnce({ statusCode: 401 })
+
+    const promise = sut({ id })
+
+    await expect(promise).rejects.toThrow(new UnauthorizedError())
   })
 })
