7bad61e6ef28fde3e622e8da77684f1666ba2015
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-07-25 15:00:20 -0300

    feat: ensure editAddressUseCase throw UnauthorizedError if HttpClient return 401

--- src/domain/use-cases/address/edit-address.ts
+++ src/domain/use-cases/address/edit-address.ts
@@ -1,4 +1,5 @@
 import { type HttpClient } from '@/domain/contracts/http'
+import { UnauthorizedError } from '@/domain/errors'
 
 type Setup = (url: string, httpClient: HttpClient) => EditAddress
 type Input = { id: string, surname?: string, number?: number, complement?: string, active?: boolean }
@@ -6,5 +7,9 @@ type Output = void
 export type EditAddress = (input: Input) => Promise<Output>
 
 export const editAddressUseCase: Setup = (url, httpClient) => async ({ id, active, complement, number, surname }) => {
-  await httpClient.request({ url, method: 'put', body: { id, active, complement, number, surname } })
+  const { statusCode } = await httpClient.request({ url, method: 'put', body: { id, active, complement, number, surname } })
+  switch (statusCode) {
+    case 204: return undefined
+    case 401: throw new UnauthorizedError()
+  }
 }
