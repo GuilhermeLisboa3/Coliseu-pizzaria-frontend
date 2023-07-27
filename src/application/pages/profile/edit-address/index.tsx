89ff6b8998a8903f752d773936a99da2857f9829
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-07-27 16:01:13 -0300

    feat: ensure Profile show spinner on submit

--- src/application/pages/profile/edit-address/index.tsx
+++ src/application/pages/profile/edit-address/index.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { Input, Button } from '@/application/components'
+import { Input, Button, Spinner } from '@/application/components'
 import { AddressContext } from '@/application/pages/profile/contexts'
 import { Form } from './style'
 import { type Address } from '@/domain/models'
@@ -14,6 +14,7 @@ type Props = { isOpen: boolean, setIsOpen: React.Dispatch<React.SetStateAction<b
 export const EditAddress: React.FC<Props> = ({ isOpen, setIsOpen, address }): JSX.Element => {
   const { validation } = useContext(AddressContext)
 
+  const [lodding, setLodding] = useState(false)
   const [number, setNumber] = useState(address.number)
   const [numberError, setNumberError] = useState<string | undefined>('')
   const [surname, setSurname] = useState(address.surname)
@@ -23,16 +24,21 @@ export const EditAddress: React.FC<Props> = ({ isOpen, setIsOpen, address }): JS
   useEffect(() => setNumberError(validation.validate('number', { number })), [number])
   useEffect(() => setSurnameError(validation.validate('surname', { surname })), [surname])
 
+  const handleEditAddress = async (event: React.FormEvent<HTMLFormElement>): Promise<void> => {
+    event.preventDefault()
+    setLodding(true)
+  }
+
   return (
     <Modal ariaHideApp={false} isOpen={isOpen} shouldCloseOnEsc={false} className='react-modal' overlayClassName='react-modal-overlay'>
       <button className='button-icon-close' onClick={() => { setIsOpen(false) }} data-testid='close-modal'><AiOutlineClose/></button>
-      <Form data-testid='edit-form'>
+      <Form data-testid='edit-form' onSubmit={handleEditAddress}>
         <div>
           <Input placeholder="Apelido" type='text' name='surname' value={surname} setState={setSurname}/>
           <Input placeholder="Número" type='text' name='number' value={number} setState={setNumber}/>
         </div>
           <Input placeholder="Complemento" type='text' name='complement' value={complement} setState={setComplement}/>
-          <Button type='submit' disabled={!!numberError || !!surnameError}>Salvar</Button>
+          <Button type='submit' disabled={!!numberError || !!surnameError}>{ lodding ? <Spinner/> : 'Salvar'}</Button>
       </Form>
     </Modal>
   )
