98f28f81827fc16624bd2d14f27d1cd795faf25e
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-07-28 17:07:19 -0300

    test: ensure listCategoryWithProductsUseCase throw UnauthorizedError if HttpClient return 401

--- tests/domain/use-cases/category/list-category-with-products.spec.ts
+++ tests/domain/use-cases/category/list-category-with-products.spec.ts
@@ -1,6 +1,7 @@
 import { type ListCategoryWithProducts, listCategoryWithProductsUseCase } from '@/domain/use-cases/category'
 import { type HttpClient } from '@/domain/contracts/http'
 import { httpClientParams } from '@/tests/mocks'
+import { UnauthorizedError } from '@/domain/errors'
 
 import { mock } from 'jest-mock-extended'
 
@@ -8,6 +9,10 @@ describe('listCategoryWithProductsUseCase', () => {
   const { url } = httpClientParams
   let sut: ListCategoryWithProducts
   const httpClient = mock<HttpClient>()
+
+  beforeAll(() => {
+    httpClient.request.mockResolvedValue({ statusCode: 200, data: {} })
+  })
 
   beforeEach(() => {
     sut = listCategoryWithProductsUseCase(url, httpClient)
@@ -18,5 +23,13 @@ describe('listCategoryWithProductsUseCase', () => {
 
     expect(httpClient.request).toHaveBeenCalledWith({ url, method: 'get' })
     expect(httpClient.request).toHaveBeenCalledTimes(1)
+  })
+
+  it('should throw UnauthorizedError if HttpClient return 401', async () => {
+    httpClient.request.mockResolvedValueOnce({ statusCode: 401 })
+
+    const promise = sut()
+
+    await expect(promise).rejects.toThrow(new UnauthorizedError())
   })
 })
