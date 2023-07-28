6227e6c776a1f099d40b241a8db1851c22708835
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-07-28 17:18:34 -0300

    feat: ensure listCategoryWithProductsUseCase return categories with products on success

---
+++ src/domain/models/products.ts
@@ -0,0 +1,9 @@
+export type Product = {
+  id: string
+  name: string
+  description: string
+  price: string
+  available: string
+  picture: string
+  categoryId: string
+}
--- src/domain/use-cases/category/list-category-with-products.ts
+++ src/domain/use-cases/category/list-category-with-products.ts
@@ -1,14 +1,15 @@
 import { type HttpClient } from '@/domain/contracts/http'
 import { UnauthorizedError, UnexpectedError } from '@/domain/errors'
+import { type Product } from '@/domain/models'
 
-type Setup = (url: string, httpClient: HttpClient) => ListCategoryWithProducts
-type Output = void
+type Setup = (url: string, httpClient: HttpClient<Array<{ id: string, name: string, products: Product[] }>>) => ListCategoryWithProducts
+type Output = Array<{ id: string, name: string, products: Product[] }>
 export type ListCategoryWithProducts = () => Promise<Output>
 
 export const listCategoryWithProductsUseCase: Setup = (url, httpClient) => async () => {
-  const { statusCode } = await httpClient.request({ url, method: 'get' })
+  const { statusCode, data } = await httpClient.request({ url, method: 'get' })
   switch (statusCode) {
-    case 200: return undefined
+    case 200: return data!
     case 401: throw new UnauthorizedError()
     default: throw new UnexpectedError()
   }
