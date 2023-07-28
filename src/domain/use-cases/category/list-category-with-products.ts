36be8b5b7ec442fb662f5b9f94942b7a01dc5876
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-07-28 16:52:29 -0300

    feat: ensure listCategoryWithProductsUseCase throw UnauthorizedError if HttpClient return 401

--- src/domain/use-cases/category/list-category-with-products.ts
+++ src/domain/use-cases/category/list-category-with-products.ts
@@ -1,9 +1,13 @@
 import { type HttpClient } from '@/domain/contracts/http'
+import { UnauthorizedError } from '@/domain/errors'
 
 type Setup = (url: string, httpClient: HttpClient) => ListCategoryWithProducts
 type Output = void
 export type ListCategoryWithProducts = () => Promise<Output>
 
 export const listCategoryWithProductsUseCase: Setup = (url, httpClient) => async () => {
-  await httpClient.request({ url, method: 'get' })
+  const { statusCode } = await httpClient.request({ url, method: 'get' })
+  switch (statusCode) {
+    case 401: throw new UnauthorizedError()
+  }
 }
