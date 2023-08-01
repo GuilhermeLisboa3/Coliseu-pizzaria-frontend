68a5af612548345c000f5490e1eda2c79bf7bd86
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-08-01 17:00:06 -0300

    feat: ensure Menu call listCategoryWithProducts

--- src/application/pages/menu/index.tsx
+++ src/application/pages/menu/index.tsx
@@ -2,10 +2,17 @@
 import { Default } from '@/application/layouts'
 import { Container } from './style'
 import { Skeleton } from './components'
+import { type ListCategoryWithProducts } from '@/domain/use-cases/category'
 
-import React from 'react'
+import React, { useEffect } from 'react'
 
-export const Menu: React.FC = (): JSX.Element => {
+type Props = {
+  listCategoryWithProducts: ListCategoryWithProducts
+}
+
+export const Menu: React.FC<Props> = ({ listCategoryWithProducts }): JSX.Element => {
+  useEffect(() => { listCategoryWithProducts() }, [])
+
   return (
     <>
       <Default>
