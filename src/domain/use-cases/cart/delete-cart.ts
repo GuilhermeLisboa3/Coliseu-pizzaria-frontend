c3ad70c9605ebe67d541a64571aa72c52e540519
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-08-03 10:03:46 -0300

    feat: ensure deleteCartUseCase throw UnexpectedError if HttpClient return 500

--- src/domain/use-cases/cart/delete-cart.ts
+++ src/domain/use-cases/cart/delete-cart.ts
@@ -1,5 +1,5 @@
 import { type HttpClient } from '@/domain/contracts/http'
-import { UnauthorizedError } from '@/domain/errors'
+import { UnauthorizedError, UnexpectedError } from '@/domain/errors'
 
 type Setup = (url: string, httpClient: HttpClient) => DeleteCart
 type Output = void
@@ -9,6 +9,8 @@ export type DeleteCart = (input: Input) => Promise<Output>
 export const deleteCartUseCase: Setup = (url, httpClient) => async ({ id }) => {
   const { statusCode } = await httpClient.request({ url: `${url}/${id}`, method: 'delete' })
   switch (statusCode) {
+    case 204: return undefined
     case 401: throw new UnauthorizedError()
+    default: throw new UnexpectedError()
   }
 }
