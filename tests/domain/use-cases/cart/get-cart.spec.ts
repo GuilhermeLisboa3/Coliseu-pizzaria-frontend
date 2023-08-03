11441233f79bc1b7dec504c30181db6e3204547e
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-08-03 13:48:03 -0300

    test: ensure getCartUseCase return cart with products on success

--- tests/domain/use-cases/cart/get-cart.spec.ts
+++ tests/domain/use-cases/cart/get-cart.spec.ts
@@ -1,17 +1,21 @@
 import { type GetCart, getCartUseCase } from '@/domain/use-cases/cart'
 import { type HttpClient } from '@/domain/contracts/http'
-import { httpClientParams } from '@/tests/mocks'
+import { httpClientParams, productParams } from '@/tests/mocks'
 
 import { mock } from 'jest-mock-extended'
 import { UnauthorizedError, UnexpectedError } from '@/domain/errors'
 
 describe('getCartUseCase', () => {
   const { url } = httpClientParams
+  const { id, available, categoryId, description, name, picture, price } = productParams
   let sut: GetCart
   const httpClient = mock<HttpClient>()
 
   beforeAll(() => {
-    httpClient.request.mockResolvedValue({ statusCode: 200, data: null })
+    httpClient.request.mockResolvedValue({
+      statusCode: 200,
+      data: { id, accountId: id, products: [{ id, available, categoryId, description, name, picture, price, quantity: 2 }] }
+    })
   })
 
   beforeEach(() => {
@@ -39,5 +43,11 @@ describe('getCartUseCase', () => {
     const promise = sut()
 
     await expect(promise).rejects.toThrow(new UnexpectedError())
+  })
+
+  it('should return cart with products on success', async () => {
+    const result = await sut()
+
+    expect(result).toEqual({ id, accountId: id, products: [{ id, available, categoryId, description, name, picture, price, quantity: 2 }] })
   })
 })
