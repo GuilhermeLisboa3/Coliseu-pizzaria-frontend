0290ca0220037e8e06887297ba713592f487a7ad
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-08-03 13:30:10 -0300

    test: ensure getCartUseCase throw UnauthorizedError if HttpClient return 401

--- tests/domain/use-cases/cart/get-cart.spec.ts
+++ tests/domain/use-cases/cart/get-cart.spec.ts
@@ -3,11 +3,16 @@ import { type HttpClient } from '@/domain/contracts/http'
 import { httpClientParams } from '@/tests/mocks'
 
 import { mock } from 'jest-mock-extended'
+import { UnauthorizedError } from '@/domain/errors'
 
 describe('getCartUseCase', () => {
   const { url } = httpClientParams
   let sut: GetCart
   const httpClient = mock<HttpClient>()
+
+  beforeAll(() => {
+    httpClient.request.mockResolvedValue({ statusCode: 200, data: null })
+  })
 
   beforeEach(() => {
     sut = getCartUseCase(url, httpClient)
@@ -18,5 +23,13 @@ describe('getCartUseCase', () => {
 
     expect(httpClient.request).toHaveBeenCalledWith({ url, method: 'get' })
     expect(httpClient.request).toHaveBeenCalledTimes(1)
+  })
+
+  it('should throw UnauthorizedError if HttpClient return 401', async () => {
+    httpClient.request.mockResolvedValueOnce({ statusCode: 401 })
+
+    const promise = sut()
+
+    await expect(promise).rejects.toThrow(new UnauthorizedError())
   })
 })
