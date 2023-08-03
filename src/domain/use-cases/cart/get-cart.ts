9d11944117b1d2df8abd21aae2ccd571bb9652f0
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-08-03 13:29:18 -0300

    feat: ensure getCartUseCase throw UnauthorizedError if HttpClient return 401

--- src/domain/use-cases/cart/get-cart.ts
+++ src/domain/use-cases/cart/get-cart.ts
@@ -1,9 +1,13 @@
 import { type HttpClient } from '@/domain/contracts/http'
+import { UnauthorizedError } from '@/domain/errors'
 
 type Setup = (url: string, httpClient: HttpClient) => GetCart
 type Output = void
 export type GetCart = () => Promise<Output>
 
 export const getCartUseCase: Setup = (url, httpClient) => async () => {
-  await httpClient.request({ url, method: 'get' })
+  const { statusCode } = await httpClient.request({ url, method: 'get' })
+  switch (statusCode) {
+    case 401: throw new UnauthorizedError()
+  }
 }
