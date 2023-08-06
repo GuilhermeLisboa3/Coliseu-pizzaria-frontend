ac7f6ac9e8368e61b985141294c641b8aadc52c9
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-08-06 12:32:31 -0300

    feat: ensure Signup keep the button disabled if form is invalid

--- tests/e2e/cypress/integration/signup.cy.ts
+++ tests/e2e/cypress/integration/signup.cy.ts
@@ -1,9 +1,30 @@
+import faker from 'faker'
+
 describe('Signup', () => {
+  const validEmail: string = faker.internet.email()
+  const invalidEmail: string = faker.random.word()
+  const password: string = faker.internet.password(8)
+  const invalidPasswordConfirmation: string = faker.internet.password(16)
+  const name: string = faker.name.findName()
+
+  const populateFields = (email = validEmail, passwordConfirmation = password): void => {
+    cy.getByTestId('name').focus().type(name)
+    cy.getByTestId('email').focus().type(email)
+    cy.getByTestId('password').focus().type(password)
+    cy.getByTestId('passwordConfirmation').focus().type(passwordConfirmation)
+  }
+
   beforeEach(() => {
     cy.visit('signup')
   })
 
   it('should load with correct initial state', () => {
     cy.getSubmitButton().should('be.disabled').should('have.text', 'Cadastre-se')
+  })
+
+  it('should keep the button disabled if form is invalid', () => {
+    populateFields(invalidEmail, invalidPasswordConfirmation)
+
+    cy.getSubmitButton().should('be.disabled')
   })
 })
