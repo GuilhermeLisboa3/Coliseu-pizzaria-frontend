b47dc32f988dbb703c05d5e8156c1c5bfec1b01a
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-08-07 16:37:30 -0300

    test: ensure AddAdrress keep the button disabled if form is invalid

--- tests/e2e/cypress/integration/add-address.cy.ts
+++ tests/e2e/cypress/integration/add-address.cy.ts
@@ -1,11 +1,35 @@
+import faker from 'faker'
+import { mockOk } from '../mocks/http-mocks'
+
 describe('AddAddress', () => {
+  const validZipCode = faker.address.zipCode('########')
+  const invalidZipCode = faker.address.zipCode('####')
+
+  const mockSuccessCart = (): void => mockOk('GET', /cart/, 'cart')
+
+  const populateSearchFormFields = (zipCode = validZipCode): void => {
+    cy.getInputById('cep').focus().type(zipCode)
+  }
+
   beforeEach(() => {
     cy.fixture('account').then(account => cy.setLocalStorageItem('account', account))
   })
 
   it('should load with correct initial state', () => {
+    mockSuccessCart()
+
     cy.visit('profile/address')
 
     cy.getSubmitButton().should('be.disabled').should('have.text', 'Buscar')
+  })
+
+  it('should keep the button disabled if form is invalid', () => {
+    mockSuccessCart()
+
+    cy.visit('profile/address')
+
+    populateSearchFormFields(invalidZipCode)
+
+    cy.getSubmitButton().should('be.disabled')
   })
 })
