7d609a515892f61fb4899401f1359dd0751ce76c
Guilherme Gonçalves Lisboa <98974275+GuilhermeLisboa3@users.noreply.github.com>
2023-08-21 19:31:25 -0300

    refactor: change api url

--- src/main/factories/infra/http/api-url.ts
+++ src/main/factories/infra/http/api-url.ts
@@ -1,1 +1,1 @@
-export const makeApiUrl = (path: string): string => `${process.env.NEXT_PUBLIC_API ?? http://localhost:3333}/${path}`
+export const makeApiUrl = (path: string): string => `${process.env.NEXT_PUBLIC_API ?? 'http://localhost:3333'}/${path}`
