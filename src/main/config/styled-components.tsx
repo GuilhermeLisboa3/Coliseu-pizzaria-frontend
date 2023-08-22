ccae726376ca91f65defb26ac3ff35439013a128
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2023-08-22 00:43:12 -0300

    fix: config styled components

--- src/main/config/styled-components.tsx
+++ src/main/config/styled-components.tsx
@@ -4,7 +4,7 @@ import React, { useState } from 'react'
 import { useServerInsertedHTML } from 'next/navigation'
 import { ServerStyleSheet, StyleSheetManager } from 'styled-components'
 
-export const StyledComponentsRegistry = ({ children }: { children: React.ReactNode }): React.ReactNode => {
+export const StyledComponentsRegistry = ({ children }: { children: React.ReactNode }): any => {
   const [styledComponentsStyleSheet] = useState(() => new ServerStyleSheet())
 
   useServerInsertedHTML(() => {
